a4835d1ad0b49e13c90d3ad3a69559fbd8ab822c
Programmer80101 <programmer80101@gmail.com>
2025-10-15 16:03:49 +0530

    update sitemap

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -2,68 +2,73 @@ import type { MetadataRoute } from "next";
 import config from "@/config";
 
 export default function sitemap(): MetadataRoute.Sitemap {
-	const pages = [
-		{
-			path: "",
-			priority: 1.0,
-			changefreq: "monthly",
-		},
-		{
-			path: "dashboard",
-			priority: 1.0,
-			changefreq: "monthly",
-		},
-		{
-			path: "account",
-			priority: 0.85,
-			changefreq: "monthly",
-		},
-		{
-			path: "auth",
-			priority: 0.85,
-			changefreq: "monthly",
-		},
-		{
-			path: "pricing",
-			priority: 0.6,
-			changefreq: "monthly",
-		},
-		{
-			path: "faq",
-			priority: 0.6,
-			changefreq: "monthly",
-		},
-		{
-			path: "contact",
-			priority: 0.3,
-			changefreq: "monthly",
-		},
-		{
-			path: "billing",
-			priority: 0.3,
-			changefreq: "monthly",
-		},
-		{
-			path: "cookie-policy",
-			priority: 0.2,
-			changefreq: "yearly",
-		},
-		{
-			path: "privacy-policy",
-			priority: 0.2,
-			changefreq: "yearly",
-		},
-		{
-			path: "terms-of-service",
-			priority: 0.2,
-			changefreq: "yearly",
-		},
-	];
+  const pages = [
+    {
+      path: "",
+      priority: 1.0,
+      changefreq: "monthly",
+    },
+    {
+      path: "dashboard",
+      priority: 1.0,
+      changefreq: "monthly",
+    },
+    {
+      path: "account",
+      priority: 0.85,
+      changefreq: "monthly",
+    },
+    {
+      path: "auth",
+      priority: 0.6,
+      changefreq: "monthly",
+    },
+    {
+      path: "pricing",
+      priority: 0.6,
+      changefreq: "monthly",
+    },
+    {
+      path: "faq",
+      priority: 0.6,
+      changefreq: "monthly",
+    },
+    {
+      path: "contact",
+      priority: 0.3,
+      changefreq: "monthly",
+    },
+    {
+      path: "billing",
+      priority: 0.3,
+      changefreq: "monthly",
+    },
+    {
+      path: "logout",
+      priority: 0.2,
+      changefreq: "monthly",
+    },
+    {
+      path: "cookie-policy",
+      priority: 0.2,
+      changefreq: "yearly",
+    },
+    {
+      path: "privacy-policy",
+      priority: 0.2,
+      changefreq: "yearly",
+    },
+    {
+      path: "terms-of-service",
+      priority: 0.2,
+      changefreq: "yearly",
+    },
+  ];
 
-	return pages.map((page) => ({
-		url: `${config.url}/${page.path}`,
-		priority: page.priority,
-		changefreq: page.changefreq,
-		lastModified: new Date().toISOString(),
-	}));
+  return pages.map((page) => ({
+    url: `${config.url}/${page.path}`,
+    priority: page.priority,
+    changefreq: page.changefreq,
+    lastModified: new Date().toISOString(),
+  }));
 }
