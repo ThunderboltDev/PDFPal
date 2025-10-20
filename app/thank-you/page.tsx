b74f3671ca8e95660cc89270f52f906a62a03377
Programmer80101 <programmer80101@gmail.com>
2025-10-20 11:50:46 +0530

    fix thank you page

--- app/thank-you/page.tsx
+++ app/thank-you/page.tsx
@@ -6,12 +6,13 @@ import {
   LayoutDashboard,
 } from "lucide-react";
 import { useSearchParams } from "next/navigation";
-import { useEffect } from "react";
+import { Suspense, useEffect } from "react";
 import { LinkButton } from "@/components/ui/button";
+import Loader from "@/components/ui/loader";
 import config from "@/config";
 import withConfetti, { type WithConfettiProps } from "@/hoc/with-confetti";
 
-function ThankYou({ triggerConfetti }: WithConfettiProps) {
+function ThankYouContent({ triggerConfetti }: WithConfettiProps) {
   const searchParams = useSearchParams();
 
   const productId = searchParams.get("product_id");
@@ -26,10 +27,16 @@ function ThankYou({ triggerConfetti }: WithConfettiProps) {
   return (
     <main className="flex items-center justify-center h-view container-lg">
       <div className="flex flex-col gap-3 justify-center items-center">
-        <CheckCircle2 className="text-success" size={72} />
+        <CheckCircle2
+          className="text-success"
+          size={72}
+        />
         <h2 className="text-center">
           Thank you for your purchase!{" "}
-          <button type="button" onClick={() => triggerConfetti()}>
+          <button
+            type="button"
+            onClick={() => triggerConfetti()}
+          >
             🎉
           </button>
         </h2>
@@ -42,17 +49,31 @@ function ThankYou({ triggerConfetti }: WithConfettiProps) {
         </p>
 
         <div className="flex flex-wrap gap-4 justify-center pt-6">
-          <LinkButton variant="accent" href="/dashboard">
+          <LinkButton
+            variant="accent"
+            href="/dashboard"
+          >
             <LayoutDashboard />
             Dashboard
           </LinkButton>
-          <LinkButton variant="primary" href="/contact">
+          <LinkButton
+            variant="primary"
+            href="/contact"
+          >
             <CircleQuestionMark />
             Need Help?
           </LinkButton>
         </div>
       </div>
     </main>
+  );
+}
+
+function ThankYou(props: WithConfettiProps) {
+  return (
+    <Suspense fallback={<Loader />}>
+      <ThankYouContent {...props} />
+    </Suspense>
   );
 }
 
