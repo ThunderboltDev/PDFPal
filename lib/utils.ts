e56a1c4d79de293d46d35b58f1ebfcded17a8cd0
Programmer80101 <programmer80101@gmail.com>
2025-11-08 20:41:10 +0530

    remove unused code

--- lib/utils.ts
+++ lib/utils.ts
@@ -4,8 +4,3 @@ import { twMerge } from "tailwind-merge";
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
 }
-
-export function getAbsoluteUrl(path: string) {
-  if (typeof window !== "undefined") return path;
-  return `${process.env.NEXT_PUBLIC_BASE_URL}${path}`;
-}
